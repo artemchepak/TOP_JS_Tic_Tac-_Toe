0a3360710dcb1714ea8154c6397b89aedb8a496a
Artem Chepak <a.chepak91@gmail.com>
2024-06-10 15:08:24 -0600

    add get user choice

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-const Gameboard = {
+let turnTracker = 0;
+
+let Gameboard = {
     gameboard: [
         [0, 0, 0],
         [0, 0, 0],
         [0, 0, 0]
     ],
+    boardMarker: ['a', 'b', 'c'],
     drawBoard: function () {
         let i = 1;
         console.log(`   a b c`);
@@ -14,5 +17,31 @@ const Gameboard = {
     }
 }
 
+let Game = {
+    startGame: function () {
+        let input = prompt('To start the game promt enter START');
+        if (input.trim().toUpperCase() === 'START') {
+            Gameboard.drawBoard();
+            this.makeChoice();
+        }
+    },
+    makeChoice: function () {
+        let userChoice = prompt('Choose cell to put your marker in. Use LetterNumber id - a1, c3, etc');
+        Gameboard.gameboard[userChoice[1]-1][Gameboard.boardMarker.indexOf(userChoice[0])] = Players.playerOne.marker;
+        Gameboard.drawBoard();
+    }
+}
 
-Gameboard.drawBoard();
+const Players = {
+    playerOne: {
+        name: "tim",
+        marker: "X"
+    },
+
+    playerTwo: {
+        name: "jenn",
+        marker: "O"
+    }
+}
+
+Game.startGame();
