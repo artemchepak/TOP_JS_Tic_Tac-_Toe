6a670945662f130237222b32df2b222b693cc834
Artem Chepak <a.chepak91@gmail.com>
2024-06-10 20:39:33 -0600

    refactor code

--- main.js
+++ main.js
@@ -1,71 +1,66 @@
 let turnTracker = 1;
 let winner = null;
 
-let Gameboard = {
+const Gameboard = {
     gameboard: [
         [0, 0, 0],
         [0, 0, 0],
         [0, 0, 0]
     ],
     boardMarker: ['a', 'b', 'c'],
-    drawBoard: function () {
-        let i = 1;
-        console.log(`   a b c`);
-        this.gameboard.forEach(element => {
-            console.log(`${i} |${element[0]}|${element[1]}|${element[2]}|`);
-            i++;
+    drawBoard() {
+        console.log('   a b c');
+        this.gameboard.forEach((row, i) => {
+            console.log(`${i + 1} |${row.join('|')}|`);
         });
+    },
+    resetBoard() {
+        this.gameboard = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ];
     }
-}
+};
 
-let Game = {
-    startGame: function () {
-        let input = prompt('To start the game promt enter START');
+const Game = {
+    startGame() {
+        const input = prompt('To start the game prompt enter START');
         if (input.trim().toUpperCase() === 'START') {
             Gameboard.drawBoard();
             this.runGame();
-        }
-        else {
+        } else {
             this.startGame();
         }
     },
-    makeChoice: function () {
-        let userChoice = prompt('Choose cell to put your marker in. Use LetterNumber id - a1, c3, etc');
-        let arrayIndexOne = userChoice[1] - 1;
-        let arrayIndexTwo = Gameboard.boardMarker.indexOf(userChoice[0]);
-        let boardCell = Gameboard.gameboard[arrayIndexOne][arrayIndexTwo];
-        if (this.userInputValidation(boardCell)) {
-            let value;
-            if (turnTracker === 1) {
-                value = Players.playerOne.marker;
-                turnTracker++;
-            } else if (turnTracker === 2) {
-                value = Players.playerTwo.marker;
-                turnTracker--;
-            }
-            Gameboard.gameboard[arrayIndexOne][arrayIndexTwo] = value;
+    makeChoice() {
+        const userChoice = prompt('Choose cell to put your marker in. Use LetterNumber id - a1, c3, etc');
+        const [letter, number] = userChoice;
+        const rowIndex = number - 1;
+        const colIndex = Gameboard.boardMarker.indexOf(letter);
+
+        if (this.isValidMove(Gameboard.gameboard[rowIndex][colIndex])) {
+            const marker = turnTracker === 1 ? Players.playerOne.marker : Players.playerTwo.marker;
+            Gameboard.gameboard[rowIndex][colIndex] = marker;
+            turnTracker = turnTracker === 1 ? 2 : 1;
+
             Gameboard.drawBoard();
-            this.checkWinner();
-            this.checkTie();
-        }
-        else {
-            alert('The cell is already chosen. Pick Another cell');
+            if (this.checkWinner() || this.checkTie()) {
+                this.restartGame();
+            }
+        } else {
+            alert('The cell is already chosen. Pick another cell');
             this.makeChoice();
         }
     },
-    runGame: function () {
+    runGame() {
         Players.pickPlayer();
         while (winner === null) {
             this.makeChoice();
         }
     },
-    userInputValidation: function (boardCell) {
-        if (boardCell === 0) {
-            return true;
-        }
-        else {
-            return false;
-        }
+    isValidMove(boardCell) {
+        return boardCell === 0;
     },
     checkWinner() {
         const lines = [
@@ -91,31 +86,19 @@ let Game = {
         }
         return false;
     },
-    checkTie: function () {
-        if (winner === null) {
-            let isItTie;
-            Gameboard.gameboard.forEach(element => {
-                if (element.includes('X') && !element.includes(0)) {
-                    isItTie = true;
-                }
-
-                if (isItTie) {
-                    console.log(`It's a tie!`);
-                    this.restartGame();
-                }
-            });
+    checkTie() {
+        if (Gameboard.gameboard.flat().every(cell => cell !== 0)) {
+            console.log(`It's a tie!`);
+            return true;
         }
+        return false;
     },
-    restartGame: function () {
+    restartGame() {
         winner = null;
-        Gameboard.gameboard = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]
-        ];
+        Gameboard.resetBoard();
         this.startGame();
     }
-}
+};
 
 const Players = {
     playerOne: {
@@ -126,19 +109,17 @@ const Players = {
         name: "jenn",
         marker: "O"
     },
-    pickPlayer: function () {
-        let input = prompt('Pick the player who plays first. Enter X or O');
+    pickPlayer() {
+        const input = prompt('Pick the player who plays first. Enter X or O');
         if (input.trim().toUpperCase() === 'X') {
             turnTracker = 1;
-        }
-        else if (input.trim().toUpperCase() === 'O') {
+        } else if (input.trim().toUpperCase() === 'O') {
             turnTracker = 2;
-        }
-        else {
+        } else {
             alert('Enter valid marker');
             this.pickPlayer();
         }
     }
-}
+};
 
 Game.startGame();
