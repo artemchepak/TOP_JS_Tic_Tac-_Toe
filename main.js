76d8525dec222e9c46da5b2157134ef2f4a59bd3
Artem Chepak <a.chepak91@gmail.com>
2024-06-10 19:04:31 -0600

    add game functionality

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let turnTracker = 0;
+let winner;
 
 let Gameboard = {
     gameboard: [
@@ -22,13 +23,91 @@ let Game = {
         let input = prompt('To start the game promt enter START');
         if (input.trim().toUpperCase() === 'START') {
             Gameboard.drawBoard();
-            this.makeChoice();
+            this.runGame();
         }
     },
     makeChoice: function () {
         let userChoice = prompt('Choose cell to put your marker in. Use LetterNumber id - a1, c3, etc');
-        Gameboard.gameboard[userChoice[1]-1][Gameboard.boardMarker.indexOf(userChoice[0])] = Players.playerOne.marker;
-        Gameboard.drawBoard();
+        let arrayIndexOne = userChoice[1] - 1;
+        let arrayIndexTwo = Gameboard.boardMarker.indexOf(userChoice[0]);
+        let boardCell = Gameboard.gameboard[arrayIndexOne][arrayIndexTwo];
+        if (this.userInputValidation(boardCell)) {
+            Gameboard.gameboard[arrayIndexOne][arrayIndexTwo] = Players.playerOne.marker;
+            Gameboard.drawBoard();
+            this.checkWinner();
+        }
+        else {
+            alert('The cell is already chosen. Pick Another cell');
+            this.makeChoice();
+        }
+    },
+    runGame: function () {
+        while (winner === undefined) {
+            this.makeChoice();
+        }
+    },
+    userInputValidation: function (boardCell) {
+        if (boardCell === 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    },
+    checkWinner: function () {
+        let lines = [];
+        //Adding horizontal lines to array of lines 
+        for (let i = 0; i < Gameboard.gameboard.length; i++) {
+            lines.push(Gameboard.gameboard[i]);
+        }
+        //Adding vertical lines to array of lines 
+        for (let columnIndex = 0; columnIndex < 3; columnIndex++) {
+            let array = [];
+            for (let i = 0; i < 3; i++) {
+                array.push(Gameboard.gameboard[i][columnIndex]);
+            }
+            lines.push(array);
+        }
+        //Adding diagonal lines to array of lines 
+        lines.push([
+            Gameboard.gameboard[0][0],
+            Gameboard.gameboard[1][1],
+            Gameboard.gameboard[2][2]
+        ]);
+        lines.push([
+            Gameboard.gameboard[2][0],
+            Gameboard.gameboard[1][1],
+            Gameboard.gameboard[0][2]
+        ]);
+    
+        function checkPlayerOne(element) {
+            return element === Players.playerOne.marker;
+        }
+        function checkPlayerTwo(element) {
+            return element === Players.playerTwo.marker;
+        }
+    
+        let winner;
+    
+        for (let i = 0; i < lines.length; i++) {
+            if (lines[i].every(checkPlayerOne)) {
+                winner = Players.playerOne.name;
+                break;
+            }
+        }
+    
+        if (!winner) {
+            for (let i = 0; i < lines.length; i++) {
+                if (lines[i].every(checkPlayerTwo)) {
+                    winner = Players.playerTwo.name;
+                    break;
+                }
+            }
+        }
+    
+        if (winner) {
+            console.log(`Winner is ${winner}`);
+        }
     }
 }
 
